8e2f68fcb497796817f9af2faa267f74eac7836e
boris-fernandez <164273833+boris-fernandez@users.noreply.github.com>
2025-12-04 02:24:23 -0500

    links

--- src/components/siderbar.jsx
+++ src/components/siderbar.jsx
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/sidebar.css";
+import { AuthContext } from "../context/AuthContext";
 
 function Sidebar({ open, setOpen }) {
+  const { isProfesor } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleHomeClick = (e) => {
+    e.preventDefault();
+    if (isProfesor === true) navigate("/teacher");
+    else navigate("/");
+    setOpen(false);
+  };
+
   return (
     <>
       {/* SIDEBAR SUPERPUESTO */}
       <aside className={open ? "overlay-sidebar open" : "overlay-sidebar"}>
-        <button className="close-btn" onClick={() => setOpen(false)}>
-          <span className="material-icons-outlined">close</span>
+        <button className='close-btn' onClick={() => setOpen(false)}>
+          <span className='material-icons-outlined'>close</span>
         </button>
 
-        <nav className="sidebar-list">
-          <a href="#">
-            <span className="material-icons-outlined">home</span> <h6>Inicio</h6>
+        <nav className='sidebar-list'>
+          <a href='/' onClick={handleHomeClick}>
+            <span className='material-icons-outlined'>home</span>{" "}
+            <h6>Inicio</h6>
           </a>
-          <a href="#">
-            <span className="material-icons-outlined">folder</span> <h6>Proyectos</h6>
+          <a href='#'>
+            <span className='material-icons-outlined'>folder</span>{" "}
+            <h6>Proyectos</h6>
           </a>
-          <a href="#">
-            <span className="material-icons-outlined">settings</span> <h6>Configuración</h6>
+          <a href='#'>
+            <span className='material-icons-outlined'>settings</span>{" "}
+            <h6>Configuración</h6>
           </a>
         </nav>
       </aside>
 
       {/* COLUMNA IZQUIERDA */}
-      <aside className="left-column">
-        <button className="menu-btn" onClick={() => setOpen(true)}>
-          <span className="material-icons-outlined">menu</span>
+      <aside className='left-column'>
+        <button className='menu-btn' onClick={() => setOpen(true)}>
+          <span className='material-icons-outlined'>menu</span>
         </button>
 
-        <nav className="icon-list">
-          <a href="#">
-            <span className="material-icons-outlined">home</span>
+        <nav className='icon-list'>
+          <a href='/' onClick={handleHomeClick}>
+            <span className='material-icons-outlined'>home</span>
           </a>
-          <a href="#">
-            <span className="material-icons-outlined">folder</span>
+          <a href='#'>
+            <span className='material-icons-outlined'>folder</span>
           </a>
-          <a href="#">
-            <span className="material-icons-outlined">settings</span>
+          <a href='#'>
+            <span className='material-icons-outlined'>settings</span>
           </a>
         </nav>
       </aside>
